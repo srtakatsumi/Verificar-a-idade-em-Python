d4567096ab9c65705da7b23d0b1d7505db20a223
Srta Katsumi <73000702+srtakatsumi@users.noreply.github.com>
2021-05-13 19:50:48 -0300

    Create prog.py

---
+++ prog.py
@@ -0,0 +1,17 @@
+while True:#para que assim que fechar o código possa repetir o código automaticamente
+
+    idade = int(input("Informe sua idade: ") )#variável para guardar a informação que o usuário irá inserir
+
+        if idade <= 5 :#se a idade informada pelo o usuário for menor que 5, irá aparecer a imagem você é um neném
+           print("Você é uma neném")
+           
+        elif idade <10 :
+           print("Você é uma criança")
+           
+        elif idade >18 :
+           print("Boa sorte com os boletos")
+           
+        else:#caso o usuário não informe um número o programa mostrará a mensagem abaixo e fechará o programa
+        
+            print("Favor inserir um valor númerico")
+            break #A instrução break finaliza a iteração e o Script continua a execução normalmente. O objetivo dessa instrução é fornecer a capacidade de forçar a interrupção da iteração
