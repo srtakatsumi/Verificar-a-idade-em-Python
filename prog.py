88e4de7e955a494977000bcf00104072a422cffb
Srta Katsumi <73000702+srtakatsumi@users.noreply.github.com>
2021-05-17 09:06:33 -0300

    Update prog.py

--- prog.py
+++ prog.py
@@ -2,13 +2,16 @@ while True:#para que assim que fechar o código possa repetir o código automati
 
     idade = int(input("Informe sua idade: ") )#variável para guardar a informação que o usuário irá inserir
 
-        if idade <= 5 :#se a idade informada pelo o usuário for menor que 5, irá aparecer a imagem você é um neném
-           print("Você é uma neném")
+        if idade => 0 && idade <= 5 : #se a idade informada pelo o usuário for menor que 5, irá aparecer a imagem você é um neném
+           print("Você é um neném")
            
-        elif idade <10 :
+        elif idade => 6 && idade < = 10 :
            print("Você é uma criança")
-           
-        elif idade >18 :
+        
+         elif idade => 11 && idade <= 17  :
+           print("Você é um adolecente")   
+         
+        elif idade => 18 :
            print("Boa sorte com os boletos")
            
         else:#caso o usuário não informe um número o programa mostrará a mensagem abaixo e fechará o programa
